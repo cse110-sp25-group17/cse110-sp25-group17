b755948bff6d4f236facda2f0d6043108887837b
Impropabletank <156865146+Impropabletank@users.noreply.github.com>
2025-05-25 15:57:48 -0700

    Added typeguard for shuffleArray
    Hope that fixes the injection sink

--- source/scripts/game.js
+++ source/scripts/game.js
@@ -35,6 +35,10 @@ async function loadPokemon() {
 
 // Shuffle array
 function shuffleArray(arr) {
+  if (!Array.isArray(arr)) {
+    throw new TypeError("Expected an array to shuffle.");
+  }
+  
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [arr[i], arr[j]] = [arr[j], arr[i]];
