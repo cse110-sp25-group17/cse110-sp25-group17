395f0c96a3f727663464ec913cb2e9e81e89fb3d
Jongwon Lee <jongwonlee12345@gmail.com>
2025-06-08 16:27:08 -0700

    removed unnecessary tests

--- __tests__/manifest.test.js
+++ __tests__/manifest.test.js
@@ -9,11 +9,8 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-describe('Manifest Link', () => {
-  test('contains a valid manifest.json link in home_page.html', () => {
-    const filePath = path.resolve(__dirname, '../source/home_page.html');
-    const html = fs.readFileSync(filePath, 'utf8');
-    const hasManifest = /<link\s+[^>]*rel=["']manifest["'][^>]*href=["']\.\.\/manifest\.json["'][^>]*>/i.test(html);
-    expect(hasManifest).toBe(true);
-  });
+test('includes manifest.json link', () => {
+  const filePath = path.resolve(__dirname, '../source/home_page.html');
+  const html = fs.readFileSync(filePath, { encoding: 'utf8' });
+  expect(html).toMatch(/<link[^>]+rel=["']manifest["'][^>]+href=["']\.\.\/manifest\.json["']/i);
 });
